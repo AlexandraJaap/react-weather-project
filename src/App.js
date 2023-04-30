fc8c5a2de7a4274da994bea163318d84f13993ae
AlexandraJaap <119429067+AlexandraJaap@users.noreply.github.com>
2023-04-30 12:32:29 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://melodious-selkie-9ed524.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Alexandra Jaap
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/AlexandraJaap/react-weather-project"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
